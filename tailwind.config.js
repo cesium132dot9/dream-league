8f098b6c0e46c08cf99836a77ed27770e0d71ae9
Justin Fung <justinfung.ca@gmail.com>
2025-11-22 14:07:31 -0500

    v2 with fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        header: ['Alagard', 'system-ui', 'sans-serif'],
+        display: ['Alagard', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
